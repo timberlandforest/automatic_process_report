440609966cff708eeaadefdbe4c5c03928b1a8de
Woodeveloops <rindolfo.barra@arauco.com>
2024-11-25 08:30:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -32,10 +32,8 @@ areas_de_proceso = {
         'reduction_lab [%]', 'alcali_lv_lab [g/L]', 'sulfidez_lab [%]'
     ],
     'Emisiones': [
-        'NOx [mg/Nm³]', 'Material particulado [mg/Nm³]', 'SO2 [mg/Nm³]',
-        'TRS [mg/Nm³]', 'CO [mg/Nm³]', 'O2_cont_left [%]',
-        'O2_cont_center [%]', 'O2_cont_right [%]', 'CO_cont_left_wall [%]',
-        'CO_cont_center [%]', 'CO_cont_right_wall [%]'
+        , 'O2_cont_left [%]', 'O2_cont_center [%]', 'O2_cont_right [%]',
+        'CO_cont_left_wall [%]', 'CO_cont_center [%]', 'CO_cont_right_wall [%]'
     ]
 }
 
